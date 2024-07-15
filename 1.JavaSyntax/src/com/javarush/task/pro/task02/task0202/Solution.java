3c78fe2746a6e129b25ce586bcb5500fc7b4e75a
Ander-starkk <0447845@gmail.com>
2024-07-15 11:11:23 +0300

    add solution JavaSyntax task0202

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0202/Solution.java
@@ -0,0 +1,33 @@
+package com.javarush.task.pro.task02.task0202;
+
+/* 
+Полнометражная картина
+*/
+import java.util.Scanner;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        String text = "10 20 40 80";
+        Scanner console = new Scanner(text);
+        int a = console.nextInt();
+        int b = console.nextInt();
+    }
+}
+    //int age = 60;
+    //boolean test = (age>65);
+    //if (test)
+      //  System.out.println("Пора на пенсию");
+        //    Scanner console = new Scanner(System.in);  // создаем объект Scanner
+       // int a = console.nextInt();  // читаем с клавиатуры первое число
+        //int b = console.nextInt();  // читаем с клавиатуры второе число
+        //if (a < b)                   // если a меньше b
+          //  System.out.println(a);     // выводим на экран a
+        //else                         // иначе
+          //  System.out.println(b);     // выводим на экран b
+      //  int centimeters = 243;
+
+        //int meters = centimeters / 100;
+
+        //System.out.println(meters);
+
