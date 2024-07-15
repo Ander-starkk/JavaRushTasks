b7a833f86fd9132bbffb6469e0ffab5c7ff791e6
Ander-starkk <0447845@gmail.com>
2024-07-15 11:11:02 +0300

    add solution JavaSyntax task0201

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.pro.task02.task0201;
+
+/* 
+«Я по объявлению…»
+*/
+
+//public class Solution {
+
+    // public static void main(String[] args) throws IOException {
+    //  int intValue = 5, numericValue = intValue + 10, size = intValue + numericValue, number = numericValue + 20
+  //  public static void main(String[] args)  {
+
+       // while (true) {
+         //   int x = System.in.read();
+        //    System.out.println(x);
+     //   }
+    //}
+//}
