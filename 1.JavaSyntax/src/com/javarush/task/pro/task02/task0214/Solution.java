0c4b3af9b1e4e0ae569ecbfcc7632baa39cb983c
Ander-starkk <0447845@gmail.com>
2024-07-15 14:40:07 +0300

    add solution JavaSyntax task0214

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0214/Solution.java
@@ -0,0 +1,55 @@
+package com.javarush.task.pro.task02.task0214;
+
+import java.util.Scanner;
+
+/* 
+Чтение и преобразование строк
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите число:");
+
+        int number = sc.nextInt();
+
+        System.out.println("Спасибо! Вы ввели число " + number);
+
+     //   sc.close();//вот теперь мы сделали все правильно!
+   //   if (sc.hasNextInt()) {
+  //        int number = sc.nextInt();
+  //        System.out.println("Спасибо! Вы ввели число " + number);
+  //    } else {
+   //       System.out.println("Извините, но это явно не число. Перезапустите программу и попробуйте снова!");
+  //    }
+
+       // Scanner scanner = new Scanner("Люблю тебя, Петра творенье,\n" +
+       //         "Люблю твой строгий, стройный вид,\n" +
+        //        "Невы державное теченье,\n" +
+       //         "Береговой ее гранит");
+      //  String s = scanner.nextLine();
+      //  System.out.println(s);
+     //   s = scanner.nextLine();
+     //   System.out.println(s);
+     //   s = scanner.nextLine();
+     //   System.out.println(s);
+     //   s = scanner.nextLine();
+     //   System.out.println(s);
+        // String prv = "5 8 20 100";
+        //Scanner tyta = new Scanner(prv);
+       // int a = tyta.nextInt();
+       // int b = tyta.nextInt();
+        //System.out.println(a + b);
+  //  Scanner stroka1 = new Scanner(System.in);
+  //  String name1 = stroka1.nextLine();
+   // Scanner stroka2 = new Scanner(System.in);
+  // String name2 = stroka2.nextLine();
+  //  Scanner stroka3 = new Scanner(System.in);
+  //  String name3 = stroka3.nextLine();
+ //   System.out.println(name3);
+ //   System.out.println(name2.toUpperCase());
+ //   System.out.println(name1.toLowerCase());
+
+    }
+}
