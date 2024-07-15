955b12b7734d770468251be440da903b84453f65
Ander-starkk <0447845@gmail.com>
2024-07-15 14:39:24 +0300

    add solution JavaSyntax task0206

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0206/Solution.java
@@ -0,0 +1,11 @@
+package com.javarush.task.pro.task02.task0206;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        String word = "Hello", phrase = "What?", line = "Red", text = "xz";
+
+    }
+}
