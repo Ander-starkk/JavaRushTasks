9f11c1987be029144be175afc4687a99d0fcd1e3
Ander-starkk <0447845@gmail.com>
2024-07-28 10:35:40 +0300

    add solution JavaSyntax task0312

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task03/task0312/Solution.java
@@ -0,0 +1,21 @@
+package com.javarush.task.pro.task03.task0312;
+
+import java.util.Scanner;
+
+/* 
+Сравним строки
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String text1 = scanner.next();
+        String text2 = scanner.next();
+        System.out.println(text1.equals(text2) ? "строки одинаковые": "строки разные") ;
+     //   if text1.equals(text2); {
+       //     System.out.println("строки одинаковые");
+       // }else{
+        //    System.out.println("строки разные");
+       // }
+        }
+}
