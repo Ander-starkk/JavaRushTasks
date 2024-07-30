937d772a717e1988c4abced89a54054aeeccf4d5
Ander-starkk <0447845@gmail.com>
2024-07-30 17:19:47 +0300

    add solution JavaSyntax task0202

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0202/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0202/Solution.java
@@ -3,36 +3,36 @@ package com.javarush.task.pro.task02.task0202;
 /* 
 Полнометражная картина
 */
+
 import java.util.Scanner;
 import java.util.Set;
 
 public class Solution {
     public static void main(String[] args) {
-        int i = 1;
-        while (i <= 20)
-        {
-            if ( (i % 7) == 0 )
-                continue;
-            System.out.println(i);
-            i++;
+        Scanner scanner = new Scanner(System.in);
+        int max = 0;
+        while (scanner.hasNextInt()) {
+            int x = scanner.nextInt();
+            if (x > max)
+                max = x;
         }
-
+        System.out.println(max);
     }
 }
-    //int age = 60;
-    //boolean test = (age>65);
-    //if (test)
-      //  System.out.println("Пора на пенсию");
-        //    Scanner console = new Scanner(System.in);  // создаем объект Scanner
-       // int a = console.nextInt();  // читаем с клавиатуры первое число
-        //int b = console.nextInt();  // читаем с клавиатуры второе число
-        //if (a < b)                   // если a меньше b
-          //  System.out.println(a);     // выводим на экран a
-        //else                         // иначе
-          //  System.out.println(b);     // выводим на экран b
-      //  int centimeters = 243;
+//int age = 60;
+//boolean test = (age>65);
+//if (test)
+//  System.out.println("Пора на пенсию");
+//    Scanner console = new Scanner(System.in);  // создаем объект Scanner
+// int a = console.nextInt();  // читаем с клавиатуры первое число
+//int b = console.nextInt();  // читаем с клавиатуры второе число
+//if (a < b)                   // если a меньше b
+//  System.out.println(a);     // выводим на экран a
+//else                         // иначе
+//  System.out.println(b);     // выводим на экран b
+//  int centimeters = 243;
 
-        //int meters = centimeters / 100;
+//int meters = centimeters / 100;
 
-        //System.out.println(meters);
+//System.out.println(meters);
 
