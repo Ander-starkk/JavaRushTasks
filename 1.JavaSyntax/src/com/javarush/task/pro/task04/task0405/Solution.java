e5e0d2c6d707c00f0f17038b4824c3232fbb4254
Ander-starkk <0447845@gmail.com>
2024-08-14 17:36:53 +0300

    add solution JavaSyntax task0404

--- 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task04/task0405/Solution.java
@@ -6,15 +6,21 @@ package com.javarush.task.pro.task04.task0405;
 
 public class Solution {
     public static void main(String[] args) {
-        int stroka1 = 0;
-        while (stroka1 < 5) {
-            int stolbec1 =0;
-            while (stolbec1 < 20) {
-                System.out.print("Б");
-                stolbec1++;
+        int vysota = 0;
+        while (vysota < 10) {
+            int shirina = 0;
+            while (shirina < 20) {
+                if (vysota == 0 || vysota == 9) {
+                    System.out.print("Б");
+                } else if (shirina == 0 || shirina == 19) {
+                    System.out.print("Б");
+                } else {
+                    System.out.print(' ');
+                }
+                shirina++;
             }
-            System.out.println("Б            Б");
-            stroka1++;
+            System.out.println();
+            vysota++;
         }
     }
 }
