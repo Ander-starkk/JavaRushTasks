e9f6f3e031b509e5683831bde005f9d0de3c77e4
Ander-starkk <0447845@gmail.com>
2024-08-19 10:37:47 +0300

    add solution JavaSyntax task0201

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -4,15 +4,25 @@ package com.javarush.task.pro.task02.task0201;
 «Я по объявлению…»
 */
 
-//public class Solution {
+import java.util.Scanner;
 
-    // public static void main(String[] args) throws IOException {
-    //  int intValue = 5, numericValue = intValue + 10, size = intValue + numericValue, number = numericValue + 20
-  //  public static void main(String[] args)  {
+public class Solution {
 
-       // while (true) {
-         //   int x = System.in.read();
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s;
+        do {
+            s = scanner.nextLine();
+        }
+        while (!s.equals("exit"));
+    }
+}
+        //  int intValue = 5, numericValue = intValue + 10, size = intValue + numericValue, number = numericValue + 20
+        //  public static void main(String[] args)  {
+
+        // while (true) {
+        //   int x = System.in.read();
         //    System.out.println(x);
-     //   }
-    //}
+        //   }
+        //}
 //}
