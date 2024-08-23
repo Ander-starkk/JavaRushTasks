3eeb376922c2a4716f932e8c1b1e8ebd7a13e925
Ander-starkk <0447845@gmail.com>
2024-08-23 12:20:41 +0300

    add solution JavaSyntax task0203

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0203/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0203/Solution.java
@@ -1,5 +1,6 @@
 package com.javarush.task.pro.task02.task0203;
 
+import java.math.BigDecimal;
 import java.util.Scanner;
 /* 
 Последняя цифра числа
@@ -8,14 +9,24 @@ import java.util.Scanner;
 public class Solution {
 
     public static void main(String[] args) {
-        double price = 6;
-        int x = (int)(5.5);
-        double t = (x * price);
-        int f = (int)(x*price);
-        double g = x/f;
-        System.out.println(x);
-        System.out.println(f);
-        System.out.println(g);
+        BigDecimal f1 = new BigDecimal(0.0);
+        BigDecimal pointOne = new BigDecimal(0.1);
+
+        for (int i = 1; i <= 11; i++) {
+            f1 = f1.add(pointOne);
+        }
+
+        BigDecimal f2 = new BigDecimal(0.1);
+        BigDecimal eleven = new BigDecimal(11);
+        f2 = f2.multiply(eleven);
+
+        System.out.println("f1 = " + f1);
+        System.out.println("f2 = " + f2);
+
+        if (f1.compareTo(f2) == 0)
+            System.out.println("f1 и f2 равны!");
+        else
+            System.out.println("f1 и f2 не равны!");
     }
 }
 //     int age = 66;
