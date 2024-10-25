e5ac3de0d9ab9b5b7cbcc66bd17f8f5a9f5d025c
Xiaomi <0447845@gmail.com>
2024-10-25 08:59:31 +0300

    add solution JavaSyntax task05003

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0501/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0501/Solution.java
@@ -1,12 +1,17 @@
 package com.javarush.task.pro.task05.task0501;
-
+import java.util.Scanner;
 /* 
 Создаем массивы
 */
 
 public class Solution {
     public static void main(String[] args) {
-     int[] intArray = new int[10];
-     double[] doubleArray = new double[10];
+        String[] array = new String[10];
+
+        array[1] = "textbezkodas";
+        array[2] = array[1];
+        array[3] = new String("Привет");
+// сравниваем
+        System.out.println(array[2]);
     }
 }
