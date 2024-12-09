5d4796784f893c3cb1fd184ef013b31652eadec9
Xiaomi <0447845@gmail.com>
2024-12-09 12:02:16 +0300

    add solution JavaSyntax task0701

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    int a;
+    byte h;
+    long y;
+    char r;
+    short j;
+    float k;
+    double l;
+    boolean sds;
+}
