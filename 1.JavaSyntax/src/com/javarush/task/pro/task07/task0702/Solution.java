4a203c3322648e2ef2b258a7ef214c5ef57f3f7a
Xiaomi <0447845@gmail.com>
2024-12-10 16:33:05 +0300

    add solution JavaSyntax task0702

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0702/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0702;
+
+/* 
+Максимальное целое
+*/
+
+public class Solution {
+    byte a = Byte.MAX_VALUE;
+    short j = Short.MAX_VALUE;
+    int k = Integer.MAX_VALUE;
+    long sada = Long.MAX_VALUE;
+}
