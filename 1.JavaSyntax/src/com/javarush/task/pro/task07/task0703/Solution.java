8f32ac03d141ffe1e07aa9fdc199cd6fd42ae0f5
Xiaomi <0447845@gmail.com>
2024-12-12 09:18:16 +0300

    add solution JavaSyntax task0703

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0703/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.pro.task07.task0703;
+
+/* 
+Минимальное целое
+*/
+
+public class Solution {
+    byte j = Byte.MIN_VALUE;
+    short k = Short.MIN_VALUE;
+    int g = Integer.MIN_VALUE;
+    long s = Long.MIN_VALUE;
+}
