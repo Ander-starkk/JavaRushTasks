9d90616d3857671e29fe3616c5a8eb1c0569021b
Xiaomi <0447845@gmail.com>
2025-01-10 10:02:05 +0300

    add solution JavaSyntax task0712

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0712/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.pro.task07.task0712;
+
+/* 
+Первые объекты
+*/
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Solution {
+    public static void main(String[] args) {
+        String s = new String();
+        String h = new String();
+        String k = new String();
+        String l = new String();
+        String q = new String();
+        String w = new String();
+        String e = new String();
+        String r = new String();
+        String t = new String();
+        String y = new String();
+        int[] array = new int[5];
+        int[] array2 = new int[6];
+        int[] array3 = new int[7];
+        int[] array4 = new int[2];
+        int[] array5 = new int[3];
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+
+    }
+}
