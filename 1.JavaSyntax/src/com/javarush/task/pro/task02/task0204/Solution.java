7fe14a342e9bad27cc1d91f2a8f0038d718ddf6d
Xiaomi <0447845@gmail.com>
2025-01-31 11:43:47 +0300

    add solution JavaSyntax task0204

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0204/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0204/Solution.java
@@ -4,13 +4,18 @@ package com.javarush.task.pro.task02.task0204;
 9-ка это перевернутая 6-ка
 */
 
+import java.util.Scanner;
+
 public class Solution {
 
     public static void main(String[] args) {
-        int i = 1;
-        while (i <= 10) {
-            System.out.println(i);
-            i++;
-        }
+        Scanner console = new Scanner(System.in);
+        String path = console.nextLine();
+
+        String[] array = path.split("/");
+        String result = String.join("\\", array);
+
+        System.out.println(result);
+
     }
 }
