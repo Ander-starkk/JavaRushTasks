44dfa284e5b8e4cfa4749608cb86f388880fa90b
Xiaomi <0447845@gmail.com>
2025-01-31 11:43:35 +0300

    add solution JavaSyntax task0201

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/Solution.java
@@ -4,30 +4,26 @@ package com.javarush.task.pro.task02.task0201;
 «Я по объявлению…»
 */
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.Scanner;
+import static java.lang.Thread.sleep;
 
 public class Solution {
+    String model;
+    int maxSpeed;
 
-    public static void main(String[] args) {
-        int[] months = {1, 2, 3, 4, 5, 6, 7, 8 ,9, 10, 11, 12};
-        fill(months, -1, 10, 8);
-        System.out.println(Arrays.toString(months));
+    public Solution(String model, int maxSpeed) {
+        this.model = model;
+        this.maxSpeed = maxSpeed;
     }
-
-    public static void fill(int[] data, int from, int to, int value)
-    {
-
-
-        if (from < 0 || to > data.length)
-            return;
-
-        for (int i = from; i < to; i++)
-        {
-            data[i] = value;
-        }
+    public static void main(String[] args) {
+        Solution bugatti = new Solution("Bugatti Veyron", 407);
+        System.out.println(bugatti.model);
+        System.out.println(bugatti.maxSpeed);
     }
 }
+
 //  int intValue = 5, numericValue = intValue + 10, size = intValue + numericValue, number = numericValue + 20
 //  public static void main(String[] args)  {
 
