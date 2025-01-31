fa0b86a7d9227f7f9552e20d261949e493867edb
Xiaomi <0447845@gmail.com>
2025-01-31 11:43:54 +0300

    add solution JavaSyntax task0205

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0205/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0205/Solution.java
@@ -4,15 +4,29 @@ package com.javarush.task.pro.task02.task0205;
 Когда-то было 32 зуба
 */
 
+import java.util.Scanner;
+
 public class Solution {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++)
+        Scanner console = new Scanner(System.in);
+        String path = console.nextLine();
+
+        if (path.endsWith(".jpg") || path.endsWith(".jpeg"))
         {
-            int starCount = 10 - i;
-            for (int j = 0; j < starCount; j++)
-                System.out.print("*");
-            System.out.println();
+            System.out.println("Это Jpeg!");
+        }
+        else if (path.endsWith(".htm") || path.endsWith(".html"))
+        {
+            System.out.println("Это HTML-страница");
+        }
+        else if (path.endsWith(".doc") || path.endsWith(".docx"))
+        {
+            System.out.println("Это документ Word");
+        }
+        else
+        {
+            System.out.println("Неизвестный формат");
         }
     }
 }
