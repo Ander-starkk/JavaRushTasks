8ba061d9ca449d3e353e8244d382cd228f59f000
Xiaomi <0447845@gmail.com>
2025-02-28 09:10:34 +0300

    add solution JavaSyntax task1001

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1001/Skyscraper.java
@@ -0,0 +1,19 @@
+package com.javarush.task.pro.task10.task1001;
+
+/* 
+Создадим небоскреб
+*/
+
+public class Skyscraper {
+
+//    public String home;
+    public Skyscraper () {
+//        this.home = "Небоскреб построен";
+        System.out.println("Небоскреб построен.");
+    }
+
+    public static void main(String[] args) {
+        Skyscraper skyscraper = new Skyscraper();
+  //      System.out.println("Небоскреб построен");
+    }
+}
