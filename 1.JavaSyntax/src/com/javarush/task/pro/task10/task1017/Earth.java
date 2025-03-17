17545ddc5b17db833bb82d272fb562f82842ff1b
Xiaomi <0447845@gmail.com>
2025-03-17 09:37:29 +0300

    add solution JavaSyntax task1017

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task10/task1017/Earth.java
@@ -0,0 +1,16 @@
+package com.javarush.task.pro.task10.task1017;
+
+/* 
+Создание материков
+*/
+
+public class Earth {
+    public static void main(String[] args) {
+        Africa africa = new Africa(123123);
+        Antarctica antarctica = new Antarctica(2132);
+        Australia australia = new Australia(3213);
+        Eurasia eurasia = new Eurasia(13213);
+        NorthAmerica northAmerica = new NorthAmerica(12321);
+        SouthAmerica southAmerica = new SouthAmerica(132);
+    }
+}
