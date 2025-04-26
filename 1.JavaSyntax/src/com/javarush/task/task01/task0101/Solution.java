8db19e7394275948d540b90008c9d6c78b148293
Xiaomi <0447845@gmail.com>
2025-04-26 11:28:01 +0300

    add solution JavaSyntax task1301

--- 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0101/Solution.java
@@ -4,8 +4,22 @@ package com.javarush.task.task01.task0101;
 Я думаю, быть программистом круто
 */
 
+import java.util.*;
+
 public class Solution {
     public static void main(String[] args) {
-        System.out.println("Я думаю, быть программистом круто");
+        HashMap<String, Integer> map = new HashMap<String, Integer>();
+        map.put("Сергей", 23);
+        map.put("Иван", 27);
+        map.put("Колян", 33);
+        map.put("Никонор", null);
+        map.put("Гелендж", 45);
+
+        for (String str: map.keySet()) {
+            Integer value = map.get(str);
+            System.out.println(str + " --> " + value);
+        }
     }
 }
+    // map.put("Сергей", 21);
+    // map.put("Николай", 34);
