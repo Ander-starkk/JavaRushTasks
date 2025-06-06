87369aca58b32940998c46af12f6a357f1381780
Xiaomi <0447845@gmail.com>
2025-06-06 08:22:22 +0300

    add solution JavaSyntax task1323

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task13/task1333/Solution.java
@@ -0,0 +1,64 @@
+package com.javarush.task.pro.task13.task1333;
+
+/* 
+Спасти рядового Компиляена
+*/
+
+import java.util.*;
+
+//public class Solution {
+
+  //  public static List<Integer> ratingsList = new ArrayList<>();
+
+    //public static void main(String[] args) {
+      //  setRatings();
+        //printList(); // before
+        //improveRatings();
+        //printList(); // after
+    //}
+
+//public static void changeNegativeNumbers() {
+      //  ArrayList ratingListCopy = new ArrayList(ratingsList);
+  //      for (Object index : ratingListCopy) {
+        //    if (index < 0) {
+        //        index = index * -1;
+    //        }
+      //  }
+   // }
+
+    //public static void changeZerosToMaximumNumber() {
+       // ArrayList ratingListCopy = new ArrayList(ratingsList);
+       // for (Integer index : ratingListCopy) {
+       //     if (index == 0) {
+        //        index = Collections.max(ratingsList);
+      //      }
+       // }
+   // }
+
+   // public static void reverseList() {
+     //   Collections.sort(ratingsList, Collections.reverseOrder());
+    //}
+
+    //public static void improveRatings() {
+      //  changeNegativeNumbers();
+       // changeZerosToMaximumNumber();
+        //reverseList();
+    //}
+
+    //private static void setRatings() {
+      //  int rating;
+        //Random random = new Random();
+        //int min = -5;
+        //int max = 11;
+
+        //for (int i = 0; i < 20; i++) {
+          //  rating = random.nextInt(max - min) + min;
+            //ratingsList.add(rating);
+        //}
+    //}
+
+    //private static void printList() {
+      //  ratingsList.forEach((value) -> System.out.print(value + " "));
+        //System.out.println();
+    //}
+//}
