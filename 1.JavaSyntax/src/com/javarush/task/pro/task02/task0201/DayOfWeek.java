e95419ac9c77cb816b044e4071550a40ef375072
Xiaomi <0447845@gmail.com>
2025-06-12 08:49:44 +0300

    add solution JavaSyntax task0201

--- 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/DayOfWeek.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task02/task0201/DayOfWeek.java
@@ -11,23 +11,54 @@ import static java.lang.Thread.sleep;
 
 public class DayOfWeek {
 
-    private String title;
+    //метод, который распечатывает все оценки
+    public static void printAllGrades(int[] grades) {
+        System.out.print("|");
+        for (int num : grades) {
 
-    public DayOfWeek(String title) {
-        this.title = title;
+            System.out.print(num + "|");
+        }
+        System.out.println();
+    }
+
+//метод, в котором выводится средняя оценка
+
+    public static double midGrade(int[] numbers) {
+        int grade = 0;
+
+        for (int num : numbers) {
+            grade = num + grade;
+        }
+        double doubleSr = (double) grade / numbers.length;
+        return doubleSr;
+
+    }
+    //метод в котором вычисляется лучшая (максимальная) оценка
+    public static int bestGrade(int[] numbers) {
+        int maxGrade = numbers[0];
+
+        for (int num : numbers) {
+            if (num > maxGrade) {
+                maxGrade = num;
+            }
+        }
+        return maxGrade;
     }
 
     public static void main(String[] args) {
-        DayOfWeek dayOfWeek = new DayOfWeek("Суббота");
-        System.out.println(dayOfWeek);
+
+//массив оценок
+        int[] grades = {5, 10, 7, 8, 9, 9, 10, 12};
+
+
+        int highest_marks = bestGrade(grades);
+        double average_marks = midGrade(grades);
+        System.out.print("All the grades: ");
+        printAllGrades(grades);
+        System.out.println("The highest grade is " + highest_marks);
+        System.out.println("The average grade is " + average_marks);
     }
 
-    @Override
-    public String toString() {
-        return "DayOfWeek{" +
-                "title='" + title + '\'' +
-                '}';
-    }
 }
 
 //  int intValue = 5, numericValue = intValue + 10, size = intValue + numericValue, number = numericValue + 20
