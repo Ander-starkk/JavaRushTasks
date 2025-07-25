dca33dff3e14cdcf6c249c3ce58ad564ada9ce5a
Xiaomi <0447845@gmail.com>
2025-07-25 09:50:21 +0300

    add solution JavaSyntax task1510

--- 1.JavaSyntax/src/com/javarush/task/pro/task15/task1510/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task15/task1510/Solution.java
@@ -15,9 +15,8 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         byte[] bytes = args[0].getBytes();
         try (InputStream stream = System.in;
-             Scanner scanner = new Scanner(stream);
-             BufferedWriter bufferedWriter = Files.newBufferedWriter(Path.of(scanner.nextLine()))) {
-           // bufferedWriter.write(bytes);
+             Scanner scanner = new Scanner(stream)) {
+            Files.write(Path.of(scanner.nextLine()), bytes);
         } catch (IOException e) {
             System.out.println("Something went wrong : " + e);
         }
